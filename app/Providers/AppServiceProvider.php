c70692139b800855c958bbb28fbda4f41741b8a2
andreputraperdana <andreputra43779@gmail.com>
2022-11-14 11:29:10 +0700

    add force https in appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('currency', function ($expression) {
             return "Rp. <?php echo number_format($expression,0,',','.'); ?>";
         });
+        if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
